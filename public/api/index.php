904b2aa226663ad22e82761a033216b3619a0c30
Constantino <constantino.antunes@gmail.com>
2015-10-06 19:53:23 +0100

    Add base UI style and api routes

--- public/api/index.php
+++ public/api/index.php
@@ -7,4 +7,26 @@
  */
 
 require_once __DIR__."/../../vendor/autoload.php";
+$config = require_once __DIR__."/../../config.php";
+$config = array_merge_recursive($config, require_once __DIR__."/../../config.local.php");
+$app = new \Slim\Slim();
+$app->view(new \JsonApiView());
+$app->add(new \JsonApiMiddleware());
 
+$app->get('/task', function () use ($app) {
+    // TODO fetch from database
+    $output = array();
+    $app->render(200, array(
+        'return' => $output
+    ));
+});
+
+$app->post('/task/add', function () use ($app) {
+    $app->render(501, array());
+});
+
+$app->post('/task/done', function () use ($app) {
+    $app->render(501, array());
+});
+
+$app->run();
