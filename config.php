ee0a4524206e00bacee4a13776693d39441578b9
Constantino <constantino.antunes@gmail.com>
2015-10-06 19:32:56 +0100

    Add base dependencies and files

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eu
+ * Date: 06/10/2015
+ * Time: 19:21
+ */
+
+return array(
+    'db' => array(
+        'host' => '127.0.0.1',
+        'name' => 'flow',
+        'user' => 'flow',
+        'pass' => 'flow',
+    ),
+);
---
+++ public/api/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eu
+ * Date: 06/10/2015
+ * Time: 19:20
+ */
+
+require_once __DIR__."/../../vendor/autoload.php";
+
